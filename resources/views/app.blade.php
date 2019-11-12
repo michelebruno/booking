301b3bbdcd3d8954ae2f0c2cb7bc5ab5f29c463a
michelebruno <bm.michelebruno@gmail.com>
2019-11-12 11:56:01 +0100

    Icone

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Booking app</title> 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+    <script src="https://kit.fontawesome.com/cb57072874.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div id="root"></div>
+    <div id="error-root"></div>
     <script src="{{ mix('js/index.js') }}"></script>
 </body>
 </html>
