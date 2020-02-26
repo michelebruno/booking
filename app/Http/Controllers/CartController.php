8796fd72ef0375b5b5f7f4aea5fa115a96f995d2
michelebruno <bm.michelebruno@gmail.com>
2020-02-26 18:14:38 +0100

    MERGE

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,11 +8,7 @@ class CartController extends Controller
 {
     public function index(Request $request)
     {
-<<<<<<< HEAD
         return view('cart');
-=======
-        # code...
->>>>>>> c0ef32c38719d1deedac4ce7a859c123798e5b9d
     }
 
     public function checkout(Request $request)
