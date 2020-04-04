34b158f5663b74e3328bb54bf9efc94f1f613852
michelebruno <bm.michelebruno@gmail.com>
2020-04-04 17:46:06 +0200

    Redis

--- app/Console/Commands/BookingInstall.php
+++ app/Console/Commands/BookingInstall.php
@@ -44,8 +44,8 @@ class BookingInstall extends Command
         $artisanCommands = array_keys(Artisan::all());
 
         if (!env("APP_KEY", false)) {
-            if (array_key_exists("key:generate", $artisanCommands)) {
-                Artisan::call("key::generate");
+            if (in_array("key:generate", $artisanCommands)) {
+                Artisan::call("key:generate");
                 $this->info("Chiave segreta generata.");
             } else $this->error("Non ho trovato il comando key:generate. Impossibile creare un app key.");
         } else $this->line("Esiste già una chiave per l'applicazione.", null, 'vvv');
@@ -62,7 +62,6 @@ class BookingInstall extends Command
         } else {
             $this->comment('Esiste già un admin. Non verrà ne verrà creato uno nuovo.', 'v');
         }
-
 
         if (in_array("passport:install", $artisanCommands)) {
             if (!Setting::isFeatureInstalled("laravel/passport")) {
--- config/database.php
+++ config/database.php
@@ -119,11 +119,22 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
+
+        'clusters' => [
+            'default' => [
+                [
+                    'host' => env('REDIS_HOST', 'localhost'),
+                    'password' => env('REDIS_PASSWORD', null),
+                    'port' => env('REDIS_PORT', 6379),
+                    'database' => 0,
+                ],
+            ],
+        ],
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
