562e29ae8ad4611571dc107c93916681220cd9e7
michelebruno <bm.michelebruno@gmail.com>
2020-04-04 15:47:18 +0200

    Autoload bug fix

--- app/Console/Commands/BookingInstall.php
+++ app/Console/Commands/BookingInstall.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 
-class BookingSetUp extends Command
+class BookingInstall extends Command
 {
     /**
      * The name and signature of the console command.
