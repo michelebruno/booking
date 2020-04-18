c93761f987f0379db6def6aef84453bf597261fc
michelebruno <bm.michelebruno@gmail.com>
2020-04-18 17:52:12 +0200

    Test

---
+++ tests/Feature/OrdineTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OrdineTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testNonAutorizzato()
+    {
+
+    }
+
+}
